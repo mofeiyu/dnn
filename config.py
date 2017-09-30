c9a4034764ebd0b80e45a6cabe68a6514cc7cdc1
mofeiyu <mofeiyu1110@foxmail.com>
2017-09-30 21:00:39 +0800

    update config

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
 #encode=utf-8
 
 import json
+import logging
 
-class Config:
+logging.basicConfig(level=logging.DEBUG)
+
+def singleton(cls):
+    instance = cls()
+    instance.__call__ = lambda: instance
+    return instance
+
+@singleton
+class Config():
     def __init__(self, conf_path='conf.json'):
         conf = json.load(file(conf_path))
-        self.data = conf['data']
         self.learning_rate = conf['learning_rate']
         self.learning_decay = conf['learning_decay']
-        self.layers_sizes = conf['layers_sizes']
+        self.hidden_layers = conf['hidden_layers']
         self.dropout = conf['dropout']
         self.batch_size = conf['batch_size']
         self.l1_regularization_strength = conf['l1_regularization_strength']
@@ -17,4 +25,10 @@ class Config:
         self.cost_fun = conf['cost_fun']
         self.opt = conf['opt']
         self.checkpoint = conf['checkpoint']
-    
+        self.steps = conf['steps']
+        logging.info("Config %s init success" % conf_path)
+        
+if __name__ == '__main__':
+    print Config().activation_function
+    print Config().batch_size
+
